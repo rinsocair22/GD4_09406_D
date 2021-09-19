896c55f4557f05ecbc96cb82d1f42767d7e645a3
ASUS <nicolausrio22@gmail.com>
2021-09-19 22:15:00 +0700

    first commit

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $name = "GD4_09406_D";
+
+
+    $con = mysqli_connect($host,$user,$pass,$name);
+
+    if(mysqli_connect_errno()){
+        echo "failed to connect to MySQL    :" . mysqli_connect_error();
+    }
+?>    
